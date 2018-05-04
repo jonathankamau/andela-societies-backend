11adceec3b1d6345d0aa3c5465af4aa274b55947
Adrian Andre <adrianadero.otieno@gmail.com>
2018-05-04 21:44:05 +0300

    [Feature #156300278] Create Role endpoint
     - Creation of the endpoint for use when manipulating the roles through the API.
     - Add the resource/endpoint to the app making available for use.
     - Created a helper function to be used within the role endpoint to paginate the data.
    
    [Chore #156300278] Test the Role endpoint
    
     - Add new test role
     - Test the roles in use for other endpoints
     - Test the role endpoint
    
    [Chore #156300278] Ensure Resources are not wasted
    
     - The PUT API request may be used even when there are no changes to be made(the edit request holds the same data as the stored resource within the API), As a result a check is made before the change is introduced to avoid use of resources for no reason.
    
    [Chore #156300278] Add test for no-change PUT request
    
     - Ensure that when the PUT request is made, the API responds with the custom response, proving that no extra resources were used.
    
    [Feature #156300278] Implement DRY code
    
     - Repeated functionality has been modularised and reused within the application
    
    [Feature #156300278] Refactor code structure
    
     - A lot of the code was not DRY. This makes it so.

---
+++ src/api/endpoints/roles.py
@@ -0,0 +1,84 @@
+"""Module for Roles."""
+
+from flask import jsonify, request
+from flask_restplus import Resource
+
+from api.utils.auth import token_required, roles_required
+from ..models import Role
+from api.utils.helpers import paginate_roles, edit_role, find_role
+from api.utils.marshmallow_schemas import role_schema
+
+
+class RoleAPI(Resource):
+    """Contain CRUD endpoints for Role."""
+
+    @token_required
+    @roles_required(["Success Ops"])
+    def post(self):
+        """Create a Role."""
+        payload = request.get_json()
+
+        result, errors = role_schema.load(payload)
+
+        if errors:
+            response = jsonify(errors)
+            status_code = role_schema.context.get('status_code')
+            role_schema.context = {'Roles': Role.query.all()}
+            response.status_code = status_code or 400
+        else:
+            role = Role(name=result['name'])
+            role.save()
+
+            response = jsonify({'message': 'Role created successfully.',
+                                'data': result})
+            response.status_code = 201
+
+        return response
+
+    @token_required
+    @roles_required(["Success Ops"])
+    def get(self, role_query=None):
+        """Get Role(s) from API."""
+        if role_query:
+            role = Role.query.get(role_query)
+            return find_role(role)
+        else:
+            search_term = request.args.get('q')
+            if search_term:
+                role = Role.query.filter_by(name=search_term).first()
+                return find_role(role)
+            else:
+                return paginate_roles()
+
+    @token_required
+    @roles_required(["Success Ops"])
+    def put(self, role_query=None):
+        """Edit a role's details."""
+        payload = request.get_json()
+
+        if payload:
+            if not role_query:
+                return {"status": "fail",
+                        "message": "Role id/name must be provided."}, 400
+
+            return edit_role(payload, role_query)
+
+    @token_required
+    @roles_required(["Success Ops"])
+    def delete(self, role_query):
+        """Delete a role."""
+        if not role_query:
+            return {"status": "fail",
+                    "message": "Role id must be provided."}, 400
+        role = Role.query.get(role_query)
+        if not role:
+            response = jsonify({"status": "fail",
+                                "message": "Role does not exist."})
+            response.status_code = 404
+            return response
+        else:
+            role.delete()
+            response = jsonify({"status": "success",
+                                "message": "Role deleted successfully."})
+            response.status_code = 200
+            return response
